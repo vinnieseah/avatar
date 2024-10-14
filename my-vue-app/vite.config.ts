64f50b7a55f0588f9c84e51bc5ec70f941d2537e
vinnieseah <166283539+vinnieseah@users.noreply.github.com>
2024-10-14 15:56:59 +0800

    inital commit

--- my-vue-app/vite.config.ts
+++
@@ -1,9 +0,0 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
-export default defineConfig({
-  plugins: [vue()],
-  base: '/avatar/',  // Make sure 'avatar' matches your GitHub repo name
-});
-
